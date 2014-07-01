23e8d4bd18afeb9d8f6fbafeded1bceba3e8fbae
Akiva Szental <amszental@gmail.com>
2014-07-01 21:22:30 +1000

    First Commit

---
+++ app/models/listing.rb
@@ -0,0 +1,8 @@
+class Listing < ActiveRecord::Base
+has_attached_file :image, :styles => { 
+:medium => "300x300>", 
+:thumb => "64x64!" }, 
+:default_url => "default.jpg" 
+validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
+
+end
